2343095e36d838b60dc07020ab3c2f1a1e27c0cf
Akhil-Samineni <akhilsamineni47@gmail.com>
2020-12-23 17:14:15 -0600

    resources commit

--- src/main/java/com/aws/util/Credentials.java
+++ src/main/java/com/aws/util/Credentials.java
@@ -12,6 +12,6 @@ public class Credentials {
 
     public static final String region ="us-east-1";
 
-    public static final String keyArn="arn:aws:kms:us-east-1:942873947245:key/f94e7580-6b24-41b6-8059-c883d473271e";
+    public static final String keyArn="arn:aws:kms:us-east-1:933928249780:key/b6db4164-c83b-4df0-957d-8d9ab5f469cb";
 
 }
